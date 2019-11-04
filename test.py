e5d7b81fe35c2c4ae84a6d4db8a30fad050e3c57
Minerva <54566780+Minerva-jiezhao@users.noreply.github.com>
2019-11-04 15:56:31 +0800

    Update and rename test_1024.py to test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import torch
 import torch.backends.cudnn as cudnn
 import torch.nn as nn
 from torch import optim
-from datasets import camvid_PG
+from datasets import data_PG
 from datasets import joint_transforms
 import torchvision.transforms as transforms
 from eval_PG import eval_net, train_net, save_weights, adjust_learning_rate, view_sample_predictions, calculateDice, save_weights_dice, weight_transform_1024
@@ -20,37 +20,21 @@ import utils.training as train_utils
 from torch.autograd import Variable
 # from unet import unet_model_PG
 from unet.unet_model import UNet1, UNet2, UNet3, UNet4
-##########configuration of GPU 
-# import os
-# import tensorflow as tf
-# from keras.backend.tensorflow_backend import set_session
-# os.environ["CUDA_VISIBLE_DEVICES"]="1"
-# config = tf.ConfigProto()
-# config.gpu_options.per_process_gpu_memory_fraction = 0.5
-# config.gpu_options.allow_growth = True
-# set_session(tf.Session(config=config))
-################
-##################################################################data
+################################################data
 
-CAMVID_PATH = Path('/home/zhaojie/PG/Pdata/')
-scaled_savepath = './datasets/scaled/'
-RESULTS_PATH = Path('results/')
-WEIGHTS_PATH = Path('./weights/7new/1024/')
-
-# RESULTS_PATH.mkdir(exist_ok=True)
-# WEIGHTS_PATH.mkdir(exist_ok=True)
+data_PATH = Path('/home/zhaojie/zhaojie/PG/Pdata/')
 batch_size = 10
-normalize = transforms.Normalize(mean=camvid_PG.mean, std=camvid_PG.std)
+normalize = transforms.Normalize(mean=data_PG.mean, std=data_PG.std)
 
 train_joint_transformer = transforms.Compose([joint_transforms.JointRandomHorizontalFlip()])
 # train_joint_transform = None
-train_dset = camvid_PG.CamVid(CAMVID_PATH, 'train', joint_transform=train_joint_transformer, transform=transforms.Compose([transforms.ToTensor(), normalize]))
+train_dset = data_PG.CamVid(data_PATH, 'train', joint_transform=train_joint_transformer, transform=transforms.Compose([transforms.ToTensor(), normalize]))
 train_loader = torch.utils.data.DataLoader(train_dset, batch_size=batch_size, shuffle=True)
 	
-val_dset = camvid_PG.CamVid(CAMVID_PATH, 'val', joint_transform=None, transform=transforms.Compose([ transforms.ToTensor(), normalize]))
+val_dset = data_PG.CamVid(data_PATH, 'val', joint_transform=None, transform=transforms.Compose([ transforms.ToTensor(), normalize]))
 val_loader = torch.utils.data.DataLoader(val_dset, batch_size=batch_size, shuffle=False)
 	
-test_dset = camvid_PG.CamVid(CAMVID_PATH, 'test', joint_transform=None, transform=transforms.Compose([ transforms.ToTensor(), normalize]))
+test_dset = data_PG.CamVid(data_PATH, 'test', joint_transform=None, transform=transforms.Compose([ transforms.ToTensor(), normalize]))
 test_loader = torch.utils.data.DataLoader(test_dset, batch_size=batch_size, shuffle=False)
 
 # print("Train: %d" %len(train_loader.dataset.imgs))
@@ -68,11 +52,7 @@ EE_size = 256
 LR = 1e-3
 net = UNet4(n_channels=3, n_classes=4).cuda()
 device = 'cuda'
-# module_dir = '/home/zhaojie/PG/PGP+/weights/train_PG_1024/PG_4_430_0.96921_0.91941_0.92652_model.pkl'
-module_dir = '/home/zhaojie/PG/PGP+/weights/train_1024/PG_4_326_0.92382_0.90794_0.90072_model.pkl'
-
-# module_dir ='/home/zhaojie/PG/PGP+/weights/train_PG_1024/PG_4_430_0.96921_0.91941_0.92652_model.pkl'
-
+module_dir = ''
 net0 = torch.load(module_dir).cuda()
 
 pretrained_dict0 = net0.state_dict()
@@ -99,16 +79,15 @@ optimizer = torch.optim.RMSprop(model.parameters(), lr=LR, weight_decay=1e-4)
 # print('EE, model', EE, model)
 pred_dir = './train_PG_pred/'
 
-FILE_test_imgs_original = '/home/zhaojie/PG/Pdata/test'
+FILE_test_imgs_original = '/home/zhaojie/zhaojie/PG/Pdata/test'
 if __name__ == '__main__':
-    EE0 = 1 #EE =1-8#2-16#3-32#4-64#5-128#6-256#7-512#8-1024#
     
     epoch_num = 2
     best_loss = 1.
     best_dice = 0.
     LR_DECAY = 0.95
     DECAY_EVERY_N_EPOCHS = 10
-    criterion = nn.NLLLoss(weight=camvid_PG.class_weight.cuda()).cuda()
+    criterion = nn.NLLLoss(weight=data_PG.class_weight.cuda()).cuda()
     
     for epoch in range(1, epoch_num):
         model = model.cuda()
